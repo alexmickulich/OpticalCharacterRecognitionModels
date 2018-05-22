0f1de8ec862a9b58bf99054b5c7f1e8662fc8362
Alexander Mikulich <almik97@mail.ru>
2018-05-23 00:49:52 +0300

    fddf

--- functions.py
+++ functions.py
@@ -158,11 +158,12 @@ class Model(object):
 		y_pred = np.argmax(exp_scores)
 		top_3 = list(zip(np.argsort(probs)[::-1][:3], np.round(probs[np.argsort(probs)[::-1][:3]] * 100, 2)))
 
+		cnn_original = CNN()
 
 		answer = str(top_3[0][0])
 		top_3_original = top_3
 		top_3_cnn = top_3
-		top_3_cnn_original =top_3
+		top_3_cnn_original = cnn_original.predict(img_array, weights='original')
 
 		answer, top_3, top_3_original, top_3_cnn, top_3_cnn_original = self.select_answer( top_3, top_3_original, top_3_cnn, top_3_cnn_original)
 		answers_dict = {'answer': str(answer), 'fnn_t': top_3, 'fnn': top_3_original, 'cnn_t': top_3_cnn, 'cnn': top_3_cnn_original}
